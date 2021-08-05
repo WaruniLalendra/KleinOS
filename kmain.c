ed191403db0350556b2684fd327b0b95c32dbb36
Waruni Lalendra <73272972+WaruniLalendra@users.noreply.github.com>
2021-08-05 10:16:01 -0700

    Display outputs (#3)

--- kmain.c
+++ kmain.c
@@ -1,1 +1,15 @@
-void kmain(){}
+
+#include "framebuffer.h"
+#include "serial.h"
+
+
+    
+void kmain(){
+
+    char msg[] = "KleinOS";
+   
+    
+    serial_write(0x3F8, msg, 7);
+    fb_write(msg, 7);
+    
+}
